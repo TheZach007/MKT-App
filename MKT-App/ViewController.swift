fdeb4a0bd85d3f508da3d55ba05c18c77af6da25
Kevin Fachal <kevin.fch@hotmail.com>
2021-12-25 14:07:56 +0700

    Initial Commit

---
+++ MKT-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MKT-App
+//
+//  Created by Phincon on 25/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MKT-AppTests/MKT_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  MKT_AppTests.swift
+//  MKT-AppTests
+//
+//  Created by Phincon on 25/12/21.
+//
+
+import XCTest
+@testable import MKT_App
+
+class MKT_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
